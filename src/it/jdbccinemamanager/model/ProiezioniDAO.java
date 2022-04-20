c4eaf2f29673ae32b100490d7b8601b1cf818be7
Davide <davidefella@users.noreply.github.com>
2022-04-20 12:57:44 +0200

    add DAO class; README

---
+++ src/it/jdbccinemamanager/model/ProiezioniDAO.java
@@ -0,0 +1,28 @@
+package it.jdbccinemamanager.model;
+
+import java.util.List;
+
+public class ProiezioniDAO {
+
+	public boolean insertProiezioni(Proiezioni new_proiezione) {
+		return true;
+	}
+
+	public List<Proiezioni> getAllProiezioni() {
+		return null;
+
+	}
+
+	public Proiezioni getProiezioniByID(int id) {
+		return null;
+	}
+
+	public boolean updateProiezioni(Proiezioni proiezioni, int old_codProiezioni) {
+		return true;
+
+	}
+
+	public boolean deleteProiezioni(int id) {
+		return true;
+	}
+}
---
+++ src/it/jdbccinemamanager/model/SalaDAO.java
@@ -0,0 +1,28 @@
+package it.jdbccinemamanager.model;
+
+import java.util.List;
+
+public class SalaDAO {
+	
+	public boolean insertSala(Sala new_sala) {
+		return true;
+	}
+
+	public List<Sala> getAllSala() {
+		return null;
+
+	}
+
+	public Sala getSalaByID(int id) {
+		return null;
+	}
+
+	public boolean updateSala(Sala sala, int old_codSala) {
+		return true;
+
+	}
+
+	public boolean deleteSala(int id) {
+		return true;
+	}
+}
